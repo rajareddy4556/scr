34d8fc08ed9979eb2fe5683f27da5549a0a454d8
Chinthala Rajasekhara Reddy <68636215+rajareddy4556@users.noreply.github.com>
2023-09-03 21:17:06 +0530

    yes

--- script.js
+++ script.js
@@ -1,43 +1,21 @@
-// script.js
-const scratchArea = document.querySelector(".scratch-area");
-const hiddenImage = document.querySelector(".hidden-image");
-
+const scratchOverlay = document.querySelector('.scratch-overlay');
 let isScratching = false;
 
-// Event listeners for touch devices
-scratchArea.addEventListener("touchstart", (e) => {
+scratchOverlay.addEventListener('mousedown', () => {
     isScratching = true;
-    handleScratch(e.touches[0].clientX, e.touches[0].clientY);
 });
 
-scratchArea.addEventListener("touchmove", (e) => {
-    if (isScratching) {
-        handleScratch(e.touches[0].clientX, e.touches[0].clientY);
-    }
-});
-
-scratchArea.addEventListener("touchend", () => {
+scratchOverlay.addEventListener('mouseup', () => {
     isScratching = false;
 });
 
-function handleScratch(x, y) {
-    const rect = scratchArea.getBoundingClientRect();
-    x -= rect.left;
-    y -= rect.top;
-
-    const revealPercent = getRevealPercentage(x, y);
-
-    if (revealPercent >= 90) {
-        hiddenImage.style.clipPath = "none"; // Fully reveal the image
-    } else {
-        // Gradually reveal the image as you scratch
-        hiddenImage.style.clipPath = `polygon(0% 0%, ${revealPercent}% 0%, ${revealPercent}% 100%, 0% 100%)`;
+scratchOverlay.addEventListener('mousemove', (e) => {
+    if (isScratching) {
+        const rect = scratchOverlay.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        
+        scratchOverlay.style.webkitMaskImage = `radial-gradient(circle at ${x}px ${y}px, transparent 10%, black 25%)`;
+        scratchOverlay.style.maskImage = `radial-gradient(circle at ${x}px ${y}px, transparent 10%, black 25%)`;
     }
-}
-
-function getRevealPercentage(x, y) {
-    const scratchAreaWidth = scratchArea.offsetWidth;
-    const scratchAreaHeight = scratchArea.offsetHeight;
-
-    return ((x / scratchAreaWidth) * 100) * ((y / scratchAreaHeight) * 100) / 100;
-}
+});
