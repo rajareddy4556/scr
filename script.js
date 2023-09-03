7ba8f60411b1f943e72f749a641ae39f46ec89ee
Chinthala Rajasekhara Reddy <68636215+rajareddy4556@users.noreply.github.com>
2023-09-03 21:20:56 +0530

    yes

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 const scratchOverlay = document.querySelector('.scratch-overlay');
 let isScratching = false;
 
-scratchOverlay.addEventListener('mousedown', () => {
+function startScratching(event) {
     isScratching = true;
-});
+    event.preventDefault();
+}
 
-scratchOverlay.addEventListener('mouseup', () => {
+function endScratching() {
     isScratching = false;
-});
+}
 
-scratchOverlay.addEventListener('mousemove', (e) => {
+function scratch(event) {
     if (isScratching) {
         const rect = scratchOverlay.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
+        let x, y;
+        
+        if (event.type === 'mousemove') {
+            x = event.clientX - rect.left;
+            y = event.clientY - rect.top;
+        } else if (event.type === 'touchmove') {
+            x = event.touches[0].clientX - rect.left;
+            y = event.touches[0].clientY - rect.top;
+        }
         
         scratchOverlay.style.webkitMaskImage = `radial-gradient(circle at ${x}px ${y}px, transparent 10%, black 25%)`;
         scratchOverlay.style.maskImage = `radial-gradient(circle at ${x}px ${y}px, transparent 10%, black 25%)`;
     }
-});
+}
+
+scratchOverlay.addEventListener('mousedown', startScratching);
+scratchOverlay.addEventListener('touchstart', startScratching);
+
+scratchOverlay.addEventListener('mouseup', endScratching);
+scratchOverlay.addEventListener('touchend', endScratching);
+
+scratchOverlay.addEventListener('mousemove', scratch);
+scratchOverlay.addEventListener('touchmove', scratch);
